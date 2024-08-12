b440931b2f7a45d26af15256af37d39a26727c93
kbedarev <kbedarev@at-consulting.ru>
2024-08-12 10:51:05 +0700

    split functions

--- src/index.ts
+++ src/index.ts
@@ -122,6 +122,9 @@ fastify.get("/testMethod", async (req: IAnyObject, reply: FastifyReply) => {
 });
 
 fastify.get("/prototype", async (req: IAnyObject, reply: FastifyReply) => {
+  let paramsArr = [];
+  let sourceMethodArr = [];
+  let sourceArr = [];
   for (const table of config.tables) {
     let dataArr = [];
     const tableColumnNames: string[] = [];
@@ -130,60 +133,39 @@ fastify.get("/prototype", async (req: IAnyObject, reply: FastifyReply) => {
     });
     const whatToSelect = tableColumnNames.join(", ");
     for (const column of table.tableColumns) {
-      type CustomFakerMethods = keyof typeof customFaker;
-      const source: CustomFakerMethods = column.source as CustomFakerMethods;
+      const source: string = column.source;
       const sourceMethod: string = column.sourceMethod;
       const sourceProps: string = column.columnProps;
-      let params;
-      //console.log(column);
-      if (column.multipleProps === "Yes") {
-        params = sourceProps.split(", ").reduce((acc, param) => {
-          const [key, value] = param.split(":");
-          acc[key.trim()] = parseInt(value.trim(), 10);
-          return acc;
-        }, {} as Record<string, number>);
-        console.log(params);
-      }
-      if (column.multipleProps === "No") {
-        try {
-          const [key, value] = sourceProps.split(": ");
-          params = { [key.trim()]: value.trim() };
-        } catch (err) {
-          console.log(err);
-        }
-      }
-
-      for (let i = 0; i < 500; i++) {
-        const data = await (customFaker as any)[source][sourceMethod](params);
-        dataArr.push(data);
-      }
-
-      // const get = await fastify.db.query(
-      //   `
-      //     DO $$
-      //     DECLARE
-      //         iteration INT := 0;
-      //     BEGIN
-      //         WHILE iteration < 10 LOOP
-      //             SELECT ${whatToSelect}
-      //             FROM public."${table.tableName}";
-      //             iteration := iteration + 1;
-      //         END LOOP;
-      //     END $$;
-      //   `
-      // );
-      //console.log("qweqeqweqewweqweq" + get);
-
-      // const update = await fastify.db.manyOrNone(
-      //   `
-      //     SELECT '${whatToSelect}' from public."${table.tableName} LIMIT 100";
-      //   `
-      // );
+      let params = getParams(column.multipleProps,sourceProps);
+      paramsArr.push(params)
+      sourceMethodArr.push(sourceMethod)
+      sourceArr.push(source)
       console.log(`SELECT '${whatToSelect}' from public."${table.tableName} LIMIT 100;`)
     }
-    console.log(dataArr[501]);
   }
+  console.log(paramsArr)
 });
+
+
+async function getParams(columnMultipleProps:string,sourceProps: string) {
+  let params;
+  if (columnMultipleProps === "Yes") {
+    params = sourceProps.split(", ").reduce((acc, param) => {
+      const [key, value] = param.split(":");
+      acc[key.trim()] = parseInt(value.trim(), 10);
+      return acc;
+    }, {} as Record<string, number>);
+  }
+  if (columnMultipleProps === "No") {
+    try {
+      const [key, value] = sourceProps.split(": ");
+      params = { [key.trim()]: value.trim() };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return params;
+}
 
 async function generateAddress() {
   const address: IAddress = {
