610a18cb3101484216bdc54b4c81d6fa6415fe31
kbedarev <kbedarev@at-consulting.ru>
2024-08-19 17:09:17 +0700

    Финальная версия

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,7 @@ fastify.get("/prototype", async (req: IAnyObject, reply: FastifyReply) => {
     }
     await Update(table.tableName, whatToSelect, columnsInTable, lenght);
   }
+  await CleanUsers()
 });
 
 async function getParams(columnMultipleProps: string, sourceProps: string) {
@@ -104,6 +105,7 @@ async function Update(
   let logu = 0;
   console.log(tableName)
   const columnNamesConcat = columnNames.replaceAll(",", ",'newWord',"); // слова, вернувшиеся из запроса будут строкой, разделенные newWord, например 'AnyanewWord1975' - anya 1975
+  console.log(columnNames)
   let params: number[] = await [];
   for (let i = 0; i < lenght; i++) {
     params[i] = i + 2;        //формирует количество параметров,выдает например 1, $2, $3
@@ -118,6 +120,7 @@ async function Update(
       LIMIT 1
       `);
     let rowBeforeUpdate: string[] = Object.values(request[0]); //Первый объект в таблице(orderBy id).
+    console.log(rowBeforeUpdate)
     let k = ["00000000-0000-0000-0000-000000000000"];
     await fastify.db.query(`DEALLOCATE PREPARE all;`);
     await fastify.db.query(`
@@ -149,6 +152,7 @@ async function Update(
        ) TABLE upd LIMIT 1;
       `); 
     while (true) {
+      let needMap:boolean[] = []
       let dataToPush: string[] = []; //массив с данными для одной строки
       for (const column of columnsInTable) { // заполнение dataToPush
         const source: string = column.source;
@@ -161,11 +165,17 @@ async function Update(
           params,
           mutateData
         );
+        if(column.needMap !== undefined && column.needMap === "no"){ // Нужно ли применять Map к данным
+          needMap.push(false)
+        }else{
+          needMap.push(true)
+        }
         dataToPush.push(data); //заполнение массива с данными для одной строки
       }
       
-      await checkMap(rowBeforeUpdate, dataToPush);
+      await checkMap(rowBeforeUpdate, dataToPush,needMap);
       let arrToResult:string[] = k.concat(dataToPush);  //?
+      console.log("arrToRes = ", arrToResult)
       const result = await fastify.db.manyOrNone(
         `EXECUTE _q($1,$${paramsS})`,
         arrToResult
@@ -173,7 +183,6 @@ async function Update(
       if (result.length === 0 || result[0].id === undefined || result[0].old === null) {
         break;
       }
-      
       rowBeforeUpdate = result[0].old.split("newWord");
       const row = result[0].id;
       k[0] = row;
@@ -197,24 +206,30 @@ async function generateData(
   mutateData: string
 ) {
   //if(checkMap())
-  let data = await (customFaker as any)[sourceArr][sourceMethodArr](paramsArr);
+  let data;
+  if(sourceArr === "NULL"){
+    data = null;
+    return data
+  }
+  data = await (customFaker as any)[sourceArr][sourceMethodArr](paramsArr);
    switch (mutateData) {
-    // case "phoneSeven":
-    //   console.log()
-    //   data = data.toString();
-    //   data = data.replace("8", "+7");  
-    //   break;
+    case "phoneSeven":
+      console.log()
+      data = data.toString();
+      data = data.replace("8", "7");
+      data = data.replace(/(\d)(\d{3})(\d{3})(\d{2})(\d{2})/, "+$1 $2 $3-$4-$5");
+      break;
   }
   return data;
 }
 
-async function checkMap(keys: any[], datas: any[]): Promise<void> {
+async function checkMap(keys: any[], datas: any[],needMap:boolean[]): Promise<void> {
   for (let i = 0;i<keys.length;i++) {
-    if(keys[i]=== ''){ // не добавлять в MAP key = '', data = сгенерированное значение
-      datas[i] = ''
+    if(keys[i]=== '' || keys[i] === null){ // не добавлять в MAP key = '', data = сгенерированное значение
+      datas[i] = null
       continue
     }
-    if (globalMap.has(keys[i])) {
+    if (globalMap.has(keys[i]) && needMap[i] === true) {
       datas[i] = globalMap.get(keys[i]); 
     } else {
       globalMap.set(keys[i], datas[i]);
@@ -222,5 +237,15 @@ async function checkMap(keys: any[], datas: any[]): Promise<void> {
   }
 }
 
+async function CleanUsers() {
+  await fastify.db.query(
+    `
+      Delete from public."Admin_Users"
+      where login != 'admin' 
+    `)
+} 
+
+
 
 startProdConnection();
+
